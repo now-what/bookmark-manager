b7dada2de343afb7fd35b24e7e07df0516e26b38
Francesco <francesco@monzo.com>
2022-02-15 18:19:59 +0100

    set up test environment for test database

--- README.md
+++ README.md
@@ -11,6 +11,13 @@ It's built with Sinatra and it uses a PostgreSQL database to store data.
 4. Connect to the database: `\c bookmark_manager;`
 5. Run the SQL query in [`/db/migrations/`](./db/migrations/01_create_bookmarks_table.sql) to create a table.
 
+# Test Database set-up
+
+1. Run `psql`
+2. Create a Test database: `CREATE DATABASE bookmark_manager_test;`
+3. Connect to the database: `\c bookmark_manager_test;`
+4. Run the SQL query in [`/db/migrations/`](./db/migrations/01_create_bookmarks_table.sql) to create a table.
+
 # User stories
 
 ## User story one
--- spec/features/bookmark_list_spec.rb
+++ spec/features/bookmark_list_spec.rb
@@ -1,5 +1,12 @@
 feature 'view the list of all bookmarks' do
     scenario 'user is presented with a list of all bookmarks' do
+
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+
+        connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+        connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+        connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
         visit "/bookmarks"
         expect(page).to have_content("http://www.makersacademy.com/")
         expect(page).to have_content("http://www.google.com/")
